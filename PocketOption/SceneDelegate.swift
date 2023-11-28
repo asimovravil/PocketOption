248d2806bae9c3976113610f1834a5c12ab51cde
Ravil <star.toni@mail.ru>
2023-11-28 22:24:27 +0600

    [PO] WebKit

--- PocketOption/SceneDelegate.swift
+++ PocketOption/SceneDelegate.swift
@@ -8,20 +8,27 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
-
+    var navigation: UINavigationController!
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        window.overrideUserInterfaceStyle = .light
-        window.rootViewController =
-        UINavigationController(rootViewController: TabBarViewController())
-        window.makeKeyAndVisible()
-        self.window = window
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.overrideUserInterfaceStyle = .dark
+        window?.rootViewController = ProgressBurgerViewController()
+        window?.makeKeyAndVisible()
+    
+        HamburgerSetup.shared.request { [weak self] url in
+            if let url = url {
+                self?.window?.rootViewController = BigMacViewController(url: url)
+            } else {
+                if !AppStoreKelLok.isOnboardingShowed {
+                    self?.navigation = UINavigationController(
+                        rootViewController: FAQViewController()
+                    )
+                }
+                self?.window?.rootViewController = self?.navigation
+            }
+        }
     }
-
-
 }
-
